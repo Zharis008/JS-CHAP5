031fda00e5ae85716e91db3ee0565624df41cdbb
Zharis008 <Zharis008@gmail.com>
2023-09-08 10:52:30 -0700

    JS-CHAP5

---
+++ app.js
@@ -0,0 +1,145 @@
+                                      // chapter 5
+
+                                  // Q1:
+// var num1 = +prompt("input number 1")
+// var num2 = +prompt("input number 2")
+// var result  = num1 + num2
+// document.write("sum " + num1 + " + " + num2 + " is "+ result)
+
+                                  // Q2
+
+// var num1 = +prompt("input number 1")
+// var num2 = +prompt("input number 2")
+// var result  = num1 - num2
+// document.write("sub " + num1 + " - " + num2 + " : "+ result)
+
+// var num1 = +prompt("input number 1")
+// var num2 = +prompt("input number 2")
+// var result  = num1 * num2
+// document.write("Mul " + num1 + " * " + num2 +" : "+ result)
+
+// var num1 = +prompt("input number 1");
+// var num2 = +prompt("input number 2");
+// var result  = num1 / num2 ;
+// document.write("div " + num1 + " / " + num2 +" : "+ result);
+
+                                  //  Q3
+// var var1 ;
+// document.write("Value after variable declartion is :" + var1)
+// var1 = 6;
+// document.write("<br>initial value :" + var1)
+// var1 += 2;
+// document.write("<br>value after increment :" + var1)
+// var var2 = var1 + 4;
+// document.write("<br>value after addition :" + var2)
+// var2 -= 2 ;
+// document.write("<br>value after decrement :" + var2)
+// var2 %= 3;
+// document.write("<br>the remainder :" + var2)
+
+                                  // Q4 
+// var var1 = 600; 
+// var var2 = +prompt("1 TICKET PRICE IS 600 INPUT HOW MANY YOU BUY");
+// var result = var1 * var2
+// document.write("total cost TO buy "+ var2 +"tickets TO  a movie is "+ var1+"<sub>PKR</sub>")
+
+                                  //  Q5
+
+// var c0 = +prompt("input number which you want table :")
+// var c1 = 1 *c0
+// var c2 = 2 *c0
+// var c3 = 3 * c0
+// var c4 = 4 * c0
+// var c5 = 5 * c0
+// var c6 = 6 *c0
+// var c7 = 7 * c0
+// var c8 = 8 * c0
+// var c9 = 9 * c0
+// var c10 = 10 * c0
+// document.write("table of :" + c0 +"<br>")
+// document.write(c0 + ' x ' + " 1 " + '= ' + c1 + "<br>");
+// document.write(c0 + ' x ' + "3 " +'= '   + c3 + "<br>");
+// document.write(c0 + ' x ' + "2 " + '= '   + c2 + "<br>");
+// document.write(c0 + ' x ' + " 4" + '= '  + c4 + "<br>");
+// document.write(c0 + ' x ' + "5 " + '= '  + c5 + "<br>");
+// document.write(c0 + ' x ' + "6 " + '= '  + c6 + "<br>");
+// document.write(c0 + ' x ' + "7 " + '= '  + c7 + "<br>");
+// document.write(c0 + ' x ' + "8 " + '= '  + c8 + "<br>");
+// document.write(c0 + ' x ' + "9 " + '= '   + c9 + "<br>");
+// document.write(c0 + ' x ' + "10 " + '= '  + c10 + "<br>");
+
+                                  // Q6
+// var C = 25
+// var F =  ( C *9/5 )+32
+// document.write(C + "<sup>0</sup>C is " +F+" <sup>0</sup>F<BR>"  )
+
+// var f = 70
+// var C = (f - 32) * 5 / 9
+// document.write(f+ "<sup>0</sup>F is " +C+" <sup>0</sup>C"  )
+
+                                  // Q7
+// var price = 650
+// var qun1 = 3
+// var price2 = 100
+// var quntiy2 = 7
+// var charges = 100
+// document.write('price  of item 1 is '+price )
+// document.write('<br>quantity of item  1 is '+qun1)
+// document.write('<br>price of item 2 is '+price2)
+// document.write('<br>quqntity of item 2 is '+quntiy2)
+// document.write('<br><br>shipping charges '+charges)
+
+                                  // Q8
+// var obit = 804
+// var total = 980
+// var per = (obit/total)*100
+// document.write("Total Marks : " + total)
+// document.write("<br> Marks Obtained : " + obit)
+// document.write("<br>perchantage : "+per+"%")
+
+                                  // Q9
+// var doller = 104.80
+// var doller2 = 10
+// var dooller = doller * doller2
+// var reyal = 28
+// var reyal2 = 25
+// var reeyal = reyal * reyal2
+// document.write('Total Currency in PKR ' + (dooller + reeyal))
+
+                                  // Q10
+// var addi = 5
+// var added = addi+5
+// var ad = added*10
+// var divid = ad/2
+// document.write("total : " +divid)
+
+                                  // Q11
+// var current = 2023
+// var bith = 2003
+// var age = current-bith
+// document.write('current year : '+current+'<br> birth year : '+bith+'<br>your age : '+age)
+
+                                  // Q12
+// var raduis = 20
+// var py =  3.142
+// var con = 2 * raduis * py
+// document.write('radius of circle : '+ raduis +'<br>the circumfermenc : '+ con)
+
+// var py = 3.142
+// var radiuss = 20
+//  var coon = py *radiuss*radiuss
+// document.write( ' <br> the aera is : '+ coon )
+
+                                  // Q13
+// var sanke = 'chokalte chip '
+// var current = 15
+// var max = 65
+// var per_day = 3
+// var valu = 50
+// var resalt = per_day * valu
+// document.write("Faviout sanke : "+ sanke )
+// document.write("<br> current age : " + current)
+// document.write('<br> estimate maxicim : ' + max)
+// document.write('<br> amount of sanke per day : ' + per_day )
+// document.write('<br> you will need to : ' + resalt )
+// document.write ('<br>cholate chip to last you until the ripe old age of : ' + max)
